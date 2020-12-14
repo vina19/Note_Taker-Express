bca960634d58ac8571be76edb4b01db895966b53
Vina Kurniasari <sj9962nf@go.minneapolis.edu>
2020-12-14 13:36:27 -0600

    Created an API GET requests

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,3 +1,13 @@
 // Linking the routes to the data source.
 const noteData = require("../db/db.json");
 
+// Routing
+module.exports = function(app) {
+    
+    // API GET Requests to get the data and show it to the user when
+    // the user visit the site
+    app.get("/api/notes", function(req, res) {
+        res.json(noteData);
+    });
+
+};
