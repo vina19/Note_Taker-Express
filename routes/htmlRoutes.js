19447214ca7ae7a3d807fdfcf56e6ab2251b5c84
Vina Kurniasari <sj9962nf@go.minneapolis.edu>
2020-12-15 11:16:32 -0600

    Set the GET * that should return the index.html

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,2 +1,10 @@
 // Include path package to get the correct file path for the html
 const path = require("path");
+
+module.exports = function(app) {
+
+    app.get("/*", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/index.html"));
+    });
+
+}
